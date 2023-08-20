dd3bb0f6938ed5cd183d2b5e1e2991e2e006fc32
MhmtMutlu <mhmetmtlu@outlook.com>
2023-08-20 22:13:11 +0300

    feat: remove tailwind and add scss files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import "@/app/assets/styles/_index.scss"
 
 const inter = Inter({ subsets: ['latin'] })
 

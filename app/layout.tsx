35388da08c6295e5d3586404a7130bde4235a9fc
MhmtMutlu <mhmetmtlu@outlook.com>
2024-04-05 02:10:40 +0300

    chore: edit keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "@/app/assets/styles/_index.scss";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,7 +9,8 @@ export const metadata: Metadata = {
   title: "sarj.dev - Elektrikli Araç Şarj İstasyonları Haritası",
   description:
     "Elektrikli araçlar için şarj istasyonlarını bulabileceğiniz interaktif harita uygulaması. sarj.dev ile şarj istasyonlarını bulun ve kullanımınızı kolaylaştırın.",
-  keywords: "sarj, sarj.dev, elektrikli araç, şarj, şarj istasyonu, ",
+  keywords:
+    "sarj, sarj.dev, elektrikli araç, şarj, şarj istasyonu, esarj, eşarj, şarjdev, şarj.dev, harita, şarj istasyonu",
   themeColor: "#000000",
   manifest: "/manifest.json",
   viewport: {
